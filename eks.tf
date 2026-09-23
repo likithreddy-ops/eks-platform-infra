e2fc00cd531695cfbd18ceb50e81734c787f4b57
likithreddy-ops <likithr.ops@gmail.com>
2026-09-22 20:47:20 -0400

    Add EBS CSI driver IAM role module

--- eks.tf
+++ eks.tf
@@ -1,3 +1,18 @@
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  role_name             = "ebs-csi-driver"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
